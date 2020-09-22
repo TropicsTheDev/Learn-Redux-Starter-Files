e1e408f879c8bc3e65e843c953ec47f875e11f44
Douglas Townsend <dougymt@gmail.com>
2020-09-22 14:17:00 -0400

    created comments component

---
+++ learn-redux/client/components/Comments.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+
+class Comments extends Component {
+  renderComment(comment, i){
+    return (
+      <div className="comment" key={i}>
+        <p>
+          <strong>{comment.user}</strong>
+          {comment.text}
+          <button className="remove-comment">&times;</button>
+        </p>
+      </div>
+    )
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    const { postId } = this.props.params;
+    const author = this.refs.author.value;
+    const comment = this.refs.comment.value;
+    this.props.addComment(postId, author, comment);
+  }
+
+  render() {
+    return (
+      <div className="comments" onSubmit={this.handleSubmit.bind(this)}>
+        {this.props.postComments.map(this.renderComment)}
+        <form ref="commentForm" className="comment-form">
+          <input type="text" ref="author" placegolder="author"/>
+          <input type="text" ref="comment" placegolder="autcommenthor"/>
+          <input type="submit" hidden/>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default Comments;
--- learn-redux/client/store/reducers/posts.js
+++ learn-redux/client/store/reducers/posts.js
@@ -3,8 +3,18 @@
 // 2. copy of state
 
 function posts(state = [], action) {
-  
-  return state;
+  switch(action.type){
+    case 'INCREMENT_LIKES' : 
+    // return updated state
+    const i = action.index;
+    return [
+      ...state.slice(0, i), // everything before the item
+      {...state[i], likes: state[i].likes + 1}, // the item
+      ...state.slice(i + 1) //everything after
+    ]
+    default: 
+    return state
+  }
 }
 
 export default posts;
